f24b0e7a8e23c6e03aa6b5c8eb2bbc578d5f0889
Allen, Scott <MAllen@tcsg.edu>
2023-08-20 11:52:37 -0400

    chg

--- aidiag.py
+++ aidiag.py
@@ -9,19 +9,6 @@ load_dotenv()
 # Set up OpenAI API key from the environment variable
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def get_log_files():
-    """Return the content of all log files in the current directory."""
-    logs = []
-    for file in os.listdir():
-        if file.endswith('.log'):
-            with open(file, 'r') as log_file:
-                logs.append(log_file.read())
-    return logs
-
-def chunk_text(text, size=2000):
-    """Divide a large text into chunks of a specified size."""
-    return [text[i:i+size] for i in range(0, len(text), size)]
-
 def stream_logs_to_chatgpt(brief):
     model_name = "gpt-3.5-turbo"
     logs = get_log_files()
@@ -29,7 +16,11 @@ def stream_logs_to_chatgpt(brief):
     for log in logs:
         chunks = chunk_text(log)
         for chunk in chunks:
-            instruction = "Summarize the main points of the following logs:" if brief else "Analyze the following logs and provide a detailed response:"
+            
+            if brief:
+                instruction = "Extract only the main issues or critical points from the following logs:"
+            else:
+                instruction = "Analyze the following logs and provide a detailed response:"
             
             messages = [
                 {
@@ -39,21 +30,35 @@ def stream_logs_to_chatgpt(brief):
                 {"role": "user", "content": f"{instruction}\n{chunk}"}
             ]
             
-            response = openai.ChatCompletion.create(
-                model=model_name,
-                messages=messages
-            )
-            
-            # Print the assistant's response (the last message in the response).
-            print(response['choices'][0]['message']['content'].strip())
+            try:
+                response = openai.ChatCompletion.create(
+                    model=model_name,
+                    messages=messages
+                )
+                # Print the assistant's response (the last message in the response).
+                print(response['choices'][0]['message']['content'].strip())
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Stream logs to ChatGPT for analysis.")
-    parser.add_argument('--brief', action='store_true', default=False, help='Only summarize the main points of the logs.')
-    parser.add_argument('--verbose', action='store_true', default=True, help='Provide a detailed response analyzing the logs (default behavior).')
+                # Check for 'q' key input
+                key_input = input("Press 'q' to stop the stream or any other key to continue...")
+                if key_input == 'q':
+                    print("Stopping the stream...")
+                    return
+            except KeyboardInterrupt:
+                print("\nStream interrupted by user.")
+                return
+
+def main():
+    parser = argparse.ArgumentParser(description="Send log files in the current directory to ChatGPT for debugging.",
+                                     epilog="During the stream of logs, you can press the 'q' key followed by 'Enter' to stop the stream, or use CTRL+C to interrupt it.")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--brief", action="store_true", help="Provide a concise summary of the main issues in the logs.")
+    group.add_argument("--verbose", action="store_true", help="Provide a detailed analysis of the logs.")
     args = parser.parse_args()
 
     if args.brief:
-        args.verbose = False
+        stream_logs_to_chatgpt(brief=True)
+    else:
+        stream_logs_to_chatgpt(brief=False)
 
-    stream_logs_to_chatgpt(args.brief)
+if __name__ == '__main__':
+    main()
